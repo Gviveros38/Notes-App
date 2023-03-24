52baa811632ca8c615d0970348e3a48cf38c757b
Gviveros38 <georgeviveros38@gmail.com>
2023-03-23 20:02:11 -0400

    Homepage defined

--- website/__init__.py
+++ website/__init__.py
@@ -5,5 +5,12 @@ def create_app():
 
     app.config['SECRET_KEY'] = 'Jakie'
 
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
     
